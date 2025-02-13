0d891773d525c1e5b176461321af8a1c822895aa
Nicolás Miñán <nm.imagenysonido@gmail.com>
2025-02-12 22:29:04 -0300

    MVP schools table

--- mta_schools/services.ts
+++ mta_schools/services.ts
@@ -1,9 +1,21 @@
-import { T_ListService } from '@/shared/service';
-import data from '@/mta_schools/fake/schoolsList.json'
+import { axiosGet } from '@/shared/data/fetchers';
+import { listService } from '@/shared/service';
 
-const schoolsList: T_ListService<I_School> = (endpoint) => {
-  return Promise.resolve(data)
+
+interface I_PaginatedResponse<T_ResultsInstance> {
+  count: number
+  next: string
+  previous: string
+  results: Array<T_ResultsInstance>
 }
+
+type T_GetSchoolsListResponse = I_PaginatedResponse<I_School>
+
+const schoolsList = listService<T_GetSchoolsListResponse>('http://127.0.0.1:8000/api/schools', axiosGet)()
 export {
   schoolsList,
+}
+
+export type {
+  T_GetSchoolsListResponse
 }
