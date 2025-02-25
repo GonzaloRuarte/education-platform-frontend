95089df38c1672b377e755280dabfe5f6298b95d
Nicolás Miñán <nm.imagenysonido@gmail.com>
2025-02-25 14:16:42 -0300

    add selectable feature to tables

--- shared/components/ListPage.tsx
+++ shared/components/ListPage.tsx
@@ -1,15 +1,14 @@
-import { DEFAULT_PAGE_SIZE } from '@/config';
-import Page from '@/shared/components/Page';
-import Table from '@/shared/components/Table';
-import { I_PaginatedResponse } from '@/shared/data/types';
-import { useInProgress } from '@/shared/hooks';
-import log from '@/shared/log';
-import { handleServiceError } from '@/shared/service';
-import { T_ServiceHook } from '@/shared/types';
-import { GridColDef, GridPaginationModel } from '@mui/x-data-grid';
-import debounce from 'debounce';
-import { useEffect, useState } from 'react';
-
+import { DEFAULT_PAGE_SIZE } from '@/config'
+import Page from '@/shared/components/Page'
+import Table from '@/shared/components/Table'
+import { I_PaginatedResponse } from '@/shared/data/types'
+import { useInProgress } from '@/shared/hooks'
+import log from '@/shared/log'
+import { handleServiceError } from '@/shared/service'
+import { T_ServiceHook } from '@/shared/types'
+import { GridColDef, GridPaginationModel } from '@mui/x-data-grid'
+import debounce from 'debounce'
+import { useEffect, useState } from 'react'
 
 interface I_Props<T_Response> {
   columns: Array<GridColDef>
@@ -24,38 +23,41 @@ function ListPage<T_Response extends I_PaginatedResponse>(p: I_Props<T_Response>
   const [paginationModel, setPaginationModel] = useState<GridPaginationModel>({
     pageSize: DEFAULT_PAGE_SIZE,
     page: 0,
-  });
-
+  })
 
   const [data, setData] = useState<T_Response | undefined>(undefined)
 
   const fetchData = () => {
-    log.info("fetching data")
+    log.info('fetching data')
     setIsInProgress(true)
     fetchingService({ page: paginationModel.page + 1, page_size: paginationModel.pageSize })
-      .then(res => {
-        setData(res);
+      .then((res) => {
+        setData(res)
       })
       .catch(handleServiceError)
-      .finally(() => { setIsInProgress(false) })
+      .finally(() => {
+        setIsInProgress(false)
+      })
   }
-
 
   useEffect(debounce(fetchData), [paginationModel])
 
-  return <Page>
-    <Page.Title>{p.title}</Page.Title>
-    <Page.Content>
-      <Table
-        data={data?.results}
-        columns={p.columns}
-        paginationModel={paginationModel}
-        onPaginationModelChange={setPaginationModel}
-        count={data?.count}
-        isLoading={isInProgress}
-      />
-    </Page.Content>
-  </Page>
+  return (
+    <Page>
+      <Page.Title>{p.title}</Page.Title>
+      <Page.Content>
+        <Table
+          data={data?.results}
+          columns={p.columns}
+          paginationModel={paginationModel}
+          onPaginationModelChange={setPaginationModel}
+          count={data?.count}
+          isLoading={isInProgress}
+          checkboxSelection
+        />
+      </Page.Content>
+    </Page>
+  )
 }
 
 export default ListPage
--- shared/components/Table.tsx
+++ shared/components/Table.tsx
@@ -1,9 +1,14 @@
-import { alpha, styled } from '@mui/material';
-import { DataGrid, DataGridProps, GridCallbackDetails, gridClasses, GridColDef, GridPaginationModel } from '@mui/x-data-grid';
+import { alpha, styled } from '@mui/material'
+import {
+  DataGrid,
+  DataGridProps,
+  GridCallbackDetails,
+  gridClasses,
+  GridColDef,
+  GridPaginationModel,
+} from '@mui/x-data-grid'
 
-
-
-const ODD_OPACITY = 0.2;
+const ODD_OPACITY = 0.2
 
 const StripedDataGrid = styled(DataGrid)(({ theme }) => ({
   [`& .${gridClasses.row}.even`]: {
@@ -15,51 +20,46 @@ const StripedDataGrid = styled(DataGrid)(({ theme }) => ({
       },
     },
     '&.Mui-selected': {
-      backgroundColor: alpha(
-        theme.palette.primary.main,
-        ODD_OPACITY + theme.palette.action.selectedOpacity,
-      ),
+      backgroundColor: alpha(theme.palette.primary.main, ODD_OPACITY + theme.palette.action.selectedOpacity),
       '&:hover': {
         backgroundColor: alpha(
           theme.palette.primary.main,
-          ODD_OPACITY +
-          theme.palette.action.selectedOpacity +
-          theme.palette.action.hoverOpacity,
+          ODD_OPACITY + theme.palette.action.selectedOpacity + theme.palette.action.hoverOpacity,
         ),
         // Reset on touch devices, it doesn't add specificity
         '@media (hover: none)': {
-          backgroundColor: alpha(
-            theme.palette.primary.main,
-            ODD_OPACITY + theme.palette.action.selectedOpacity,
-          ),
+          backgroundColor: alpha(theme.palette.primary.main, ODD_OPACITY + theme.palette.action.selectedOpacity),
         },
       },
     },
   },
-}));
+}))
 
-interface I_Props {
+type I_SelectedDataGridProps = Pick<DataGridProps, 'checkboxSelection'>
+
+interface I_Props extends I_SelectedDataGridProps {
   paginationModel: GridPaginationModel
-  onPaginationModelChange: ((model: GridPaginationModel, details: GridCallbackDetails<"pagination">) => void) | undefined
-  data?: DataGridProps["rows"]
+  onPaginationModelChange:
+    | ((model: GridPaginationModel, details: GridCallbackDetails<'pagination'>) => void)
+    | undefined
+  data?: DataGridProps['rows']
   count?: number
   columns: Array<GridColDef>
   isLoading?: boolean
 }
 
-function Table({ isLoading = false, ...p }: I_Props) {
-  return <StripedDataGrid
-    rows={p.data} columns={p.columns}
-    getRowClassName={(params) =>
-      params.indexRelativeToCurrentPage % 2 === 0 ? 'even' : 'odd'
-    }
-    paginationModel={p.paginationModel}
-    pageSizeOptions={[10, 25, 100]}
-    onPaginationModelChange={p.onPaginationModelChange}
-    paginationMode='server'
-    loading={isLoading}
-    rowCount={p.count || 0}
-  />
+function Table({ isLoading = false, data, count, ...props }: I_Props) {
+  return (
+    <StripedDataGrid
+      rows={data}
+      getRowClassName={(params) => (params.indexRelativeToCurrentPage % 2 === 0 ? 'even' : 'odd')}
+      pageSizeOptions={[10, 25, 100]}
+      paginationMode="server"
+      loading={isLoading}
+      rowCount={count || 0}
+      {...props}
+    />
+  )
 }
 
 export default Table
