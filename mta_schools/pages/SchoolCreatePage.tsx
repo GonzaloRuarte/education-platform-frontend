df8da4e8f7cde8b2db785a0ef52497adece63302
Nicolás Miñán <nm.imagenysonido@gmail.com>
2025-02-28 09:52:22 -0300

    improve SchoolCreatePage

--- mta_schools/pages/SchoolCreatePage.tsx
+++ mta_schools/pages/SchoolCreatePage.tsx
@@ -1,10 +1,21 @@
+'use client'
+
 import SchoolCreateForm from '@/mta_schools/components/SchoolCreateForm'
+import { useNavigateToSchoolList } from '@/mta_schools/hooks'
+import Button from '@/shared/components/Button'
 import Page from '@/shared/components/Page'
+import ClearIcon from '@mui/icons-material/Clear'
 
 const SchoolCreatePage = () => {
+  const n = useNavigateToSchoolList()
   return (
     <Page>
       <Page.Title>Agregar Escuela</Page.Title>
+      <Page.Toolbar>
+        <Button onClick={n} startIcon={<ClearIcon />}>
+          Cancelar
+        </Button>
+      </Page.Toolbar>
       <Page.Content>
         <SchoolCreateForm />
       </Page.Content>
