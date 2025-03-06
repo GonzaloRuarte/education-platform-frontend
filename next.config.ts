d7e4454c2385b1880beda12585e9071130bbcae3
Nicolás Miñán <nm.imagenysonido@gmail.com>
2025-03-06 09:41:31 -0300

    update package.json and next.config.ts for Serwist integration and improve TypeScript configuration

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,28 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+import withSerwistInit from '@serwist/next'
+import crypto from 'crypto'
+
+const revision = crypto.randomUUID()
+
+const withSerwist = withSerwistInit({
+  cacheOnNavigation: true,
+  swSrc: 'app/sw.ts',
+  swDest: 'public/sw.js',
+  additionalPrecacheEntries: [{ url: '/~offline', revision }],
+})
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      // Basic redirect
-      // {
-      //   source: '/',
-      //   destination: '/dashboard/',
-      //   permanent: false,
-      // },
+  reactStrictMode: true,
+  // async redirects() {
+  //   return [
+  //     Basic redirect
+  //     {
+  //       source: '/',
+  //       destination: '/dashboard/',
+  //       permanent: false,
+  //     },
+  //   ]
+  // },
+}
 
-    ]
-  }
-};
-
-export default nextConfig;
+export default withSerwist(nextConfig)
