4968846acb01bf4dd701e6ab1627bceb139449d8
Nicolás Miñán <nm.imagenysonido@gmail.com>
2025-03-06 17:24:48 -0300

    add EvaluationCreateForm and EvaluationEditPage components

---
+++ mta_evaluations/pages/EvaluationEditPage.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import EvaluationEditForm from '@/mta_evaluations/components/EvaluationEditForm'
+import { useEvaluationDelete, useEvaluationDetail, useNavigateToEvaluationList } from '@/mta_evaluations/hooks'
+import EditionPage from '@/shared/pages/EditionPage'
+
+const EvaluationEditPage = () => (
+  <EditionPage
+    EditionForm={EvaluationEditForm}
+    entityName="Evaluación"
+    useDelete={useEvaluationDelete}
+    useDetail={useEvaluationDetail}
+    useNavigateToList={useNavigateToEvaluationList}
+  />
+)
+
+export default EvaluationEditPage
