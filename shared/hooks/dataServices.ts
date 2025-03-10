572f93023f3aa4d992c2111a0d1c6039f23e66b3
Nicolás Miñán <nm.imagenysonido@gmail.com>
2025-03-10 12:18:41 -0300

    add detailHookV2 for enhanced data fetching with updated service

--- shared/hooks/dataServices.ts
+++ shared/hooks/dataServices.ts
@@ -1,12 +1,13 @@
 import { I_AuthResources } from '@/mta_auth/types'
 import { I_FetchOptions, T_DeleteMethod, T_GetMethod, T_PatchMethod, T_PostMethod } from '@/shared/data/types'
-import { batchDeletionService, deletionService, detailService, listService, listService2, postService, updateService } from '@/shared/service'
+import { batchDeletionService, deletionService, detailService, detailService2, listService, listService2, postService, updateService } from '@/shared/service'
 import {
   I_DeletionCommonResponse,
   T_BatchDeletionServiceHook,
   T_CreateServiceHook,
   T_DeletionServiceHook,
   T_DetailServiceHook,
+  T_DetailServiceHookV2,
   T_ListServiceHook,
   T_ListServiceHookV2,
   T_UpdateServiceHook,
@@ -65,6 +66,20 @@ const detailHook = <T_Id, T_Response>(entityPath: string, getMethod: T_DeleteMet
   return useDetail
 }
 
+const detailHookV2 = <T_Id, T_Response>(entityPath: string, getMethod: T_GetMethod, useAuthResources: () => I_AuthResources) => {
+  const useList: T_DetailServiceHookV2<T_Id, T_Response> = (id: T_Id, options?: I_FetchOptions) => {
+    const [data, setData] = useState<undefined | T_Response>(undefined)
+    const fetcher = detailService2<T_Id, T_Response>(entityPath, getMethod)(id, useAuthResources(), options)
+    const reload = () => {
+      fetcher().then((res) => setData(res))
+    }
+    useEffect(reload, [options, entityPath])
+
+    return { data, reload }
+  }
+  return useList
+}
+
 const updateHook = <T_Id, T_RequestData, T_Response>(entityPath: string, patchMethod: T_PatchMethod, useAuthResources: () => I_AuthResources) => {
   const useUpdate: T_UpdateServiceHook<T_Id, T_RequestData, T_Response> = () => {
     return updateService<T_Id, T_RequestData, T_Response>(entityPath, patchMethod)(useAuthResources())
@@ -72,4 +87,4 @@ const updateHook = <T_Id, T_RequestData, T_Response>(entityPath: string, patchMe
   return useUpdate
 }
 
-export { batchDeletionHook, creationHook, deletionHook, detailHook, listHook, listHookV2, updateHook }
+export { batchDeletionHook, creationHook, deletionHook, detailHook, listHook, listHookV2, detailHookV2, updateHook }
