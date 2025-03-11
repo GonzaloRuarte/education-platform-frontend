e8d727d34eb53ad410431adef0420a76f218e3fc
Nicolás Miñán <nm.imagenysonido@gmail.com>
2025-03-11 08:14:59 -0300

    refactor base pages

--- shared/pages/EditionPage.tsx
+++ shared/pages/EditionPage.tsx
@@ -3,51 +3,52 @@ import Page from '@/shared/components/Page'
 import Spinner from '@/shared/components/Spinner'
 import { useConfirm } from '@/shared/confirm'
 import { useHandleDelete } from '@/shared/hooks'
-import { T_DeletionServiceHook, T_DetailServiceHook, T_NavigateToListHook } from '@/shared/types'
+import { T_DeletionServiceHook, T_DetailServiceHookV2, T_NavigateToListHook } from '@/shared/types'
 import ClearIcon from '@mui/icons-material/Clear'
 import DeleteIcon from '@mui/icons-material/Delete'
 import { useParams } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+
+const DeleteButton = ({ useDelete, id, callback, entityName }) => {
+  const deleteInstance = useDelete()
+  const { ConfirmDialogComponent, showConfirm } = useConfirm()
+  const handleDelete = useHandleDelete(id, { showConfirm, deleteInstance, callback, entityName })
+  return (
+    <>
+      <Button onClick={handleDelete} startIcon={<DeleteIcon />}>
+        Eliminar
+      </Button>
+      <ConfirmDialogComponent />
+    </>
+  )
+}
 
 interface I_Props<T_Id, T_Data> {
   EditionForm: React.ComponentType<{ data: T_Data }>
   entityName: string
-  useDetail: T_DetailServiceHook<T_Id, T_Data>
-  useDelete: T_DeletionServiceHook<T_Id, any>
-  useNavigateToList: T_NavigateToListHook
+  useDetail: T_DetailServiceHookV2<T_Id, T_Data>
+  useDelete?: T_DeletionServiceHook<T_Id, any>
+  onExit: () => void
   idFieldName?: string
 }
 
 export default function EditionPage<T_Id extends string | number, T_Data>({ idFieldName = 'id', ...p }: I_Props<T_Id, T_Data>) {
   const urlParams = useParams()
   const id = urlParams[idFieldName] as T_Id
-  const detail = p.useDetail()
-  const deleteInstance = p.useDelete()
-  const navigateToList = p.useNavigateToList()
-  const [data, setData] = useState<T_Data | undefined>(undefined)
+  const { data } = p.useDetail(id)
 
-  const { ConfirmDialogComponent, showConfirm } = useConfirm()
-
-  const handleDelete = useHandleDelete(id, { showConfirm, deleteInstance, callback: navigateToList, entityName: p.entityName })
-
-  useEffect(() => {
-    detail(id).then(setData)
-  }, [id])
   return (
     <>
       <Page>
         <Page.Title>Editar {p.entityName}</Page.Title>
         <Page.Toolbar>
-          <Button onClick={navigateToList} startIcon={<ClearIcon />}>
+          <Button onClick={p.onExit} startIcon={<ClearIcon />}>
             Cancelar
           </Button>
-          <Button onClick={handleDelete} startIcon={<DeleteIcon />}>
-            Eliminar
-          </Button>
+          {p.useDelete !== undefined && <DeleteButton callback={p.onExit} entityName={p.entityName} useDelete={p.useDelete} id={id} />}
         </Page.Toolbar>
         <Page.Content>{data === undefined ? <Spinner /> : <p.EditionForm data={data} />}</Page.Content>
       </Page>
-      <ConfirmDialogComponent />
     </>
   )
 }
--- shared/pages/ListPage.tsx
+++ shared/pages/ListPage.tsx
@@ -3,32 +3,26 @@ import Button from '@/shared/components/Button'
 import Page from '@/shared/components/Page'
 import Table from '@/shared/components/Table'
 import { I_PaginatedResponse } from '@/shared/data/types'
-import { useInProgressLocal } from '@/shared/hooks'
-import log from '@/shared/log'
-import { handleServiceError } from '@/shared/service'
-import { T_ListServiceHook } from '@/shared/types'
+import { T_ListServiceHookV2 } from '@/shared/types'
 import AddCircleIcon from '@mui/icons-material/AddCircle'
 import DeleteIcon from '@mui/icons-material/Delete'
 import ReplayIcon from '@mui/icons-material/Replay'
-import { GridColDef, GridPaginationModel, GridRowParams, GridRowSelectionModel, GridToolbarContainer, GridToolbarProps } from '@mui/x-data-grid'
+import { GridColDef, GridPaginationModel, GridRowParams, GridRowSelectionModel } from '@mui/x-data-grid'
 
 import { useConfirm } from '@/shared/confirm'
 import { successToast } from '@/shared/toasts'
-import debounce from 'debounce'
-import { ComponentProps, useEffect, useState } from 'react'
+import { ComponentProps, useState } from 'react'
 
 interface I_Props<T_Response> {
   columns: Array<GridColDef>
-  useList: T_ListServiceHook<T_Response>
-  title: string
+  useList: T_ListServiceHookV2<T_Response>
+  entityName: string
   onRowClick?: ComponentProps<typeof Table>['onRowClick']
   onCreate?: () => void
   onBatchDelete?: (ids: Array<number | string>) => Promise<any>
 }
 function ListPage<T_Response extends I_PaginatedResponse>(p: I_Props<T_Response>) {
-  const { isInProgress, setIsInProgress } = useInProgressLocal()
   const { ConfirmDialogComponent, showConfirm } = useConfirm()
-  const fetchingService = p.useList()
 
   const [paginationModel, setPaginationModel] = useState<GridPaginationModel>({
     pageSize: DEFAULT_PAGE_SIZE,
@@ -36,43 +30,40 @@ function ListPage<T_Response extends I_PaginatedResponse>(p: I_Props<T_Response>
   })
   const [rowSelectionModel, setRowSelectionModel] = useState<GridRowSelectionModel>([])
   const showDelete = rowSelectionModel.length > 0
-
-  const [data, setData] = useState<T_Response | undefined>(undefined)
+  const { data, isLoading, reload } = p.useList({ page: paginationModel.page + 1, page_size: paginationModel.pageSize })
 
   const handleBatchDelete = () => {
     const ids = rowSelectionModel as Array<number>
 
-    showConfirm(`Eliminar escuelas ${ids}`, '¿Estás seguro/a que querés eliminar estas Escuelas?').then(() => {
+    showConfirm(`Eliminar ${p.entityName} ${ids}`, '¿Estás seguro/a que querés eliminar estas Escuelas?').then(() => {
       if (p.onBatchDelete === undefined) return
       p.onBatchDelete(ids)
         .then(() => {
           successToast('Escuelas eliminadas correctamente.')
         })
-        .finally(fetchData)
+        .finally(reload)
     })
   }
 
-  const fetchData = () => {
-    log.info('fetching data')
-    setIsInProgress(true)
-    fetchingService({ page: paginationModel.page + 1, page_size: paginationModel.pageSize })
-      .then((res) => {
-        setData(res)
-      })
-      .catch(handleServiceError)
-      .finally(() => {
-        setIsInProgress(false)
-      })
-  }
-
-  useEffect(debounce(fetchData), [paginationModel])
+  // const fetchData = () => {
+  //   log.info('fetching data')
+  //   setIsInProgress(true)
+  //   fetchingService({ page: paginationModel.page + 1, page_size: paginationModel.pageSize })
+  //     .then((res) => {
+  //       setData(res)
+  //     })
+  //     .catch(handleServiceError)
+  //     .finally(() => {
+  //       setIsInProgress(false)
+  //     })
+  // }
 
   return (
     <>
       <Page>
-        <Page.Title>{p.title}</Page.Title>
+        <Page.Title>Listado de {p.entityName}</Page.Title>
         <Page.Toolbar>
-          <Button onClick={fetchData} startIcon={<ReplayIcon />}>
+          <Button onClick={reload} startIcon={<ReplayIcon />}>
             Actualizar
           </Button>
           {p.onCreate !== undefined && (
@@ -95,7 +86,7 @@ function ListPage<T_Response extends I_PaginatedResponse>(p: I_Props<T_Response>
             onRowSelectionModelChange={setRowSelectionModel}
             rowSelectionModel={rowSelectionModel}
             count={data?.count}
-            isLoading={isInProgress}
+            isLoading={isLoading}
             checkboxSelection
             disableRowSelectionOnClick
             onRowClick={p.onRowClick}
@@ -107,16 +98,16 @@ function ListPage<T_Response extends I_PaginatedResponse>(p: I_Props<T_Response>
   )
 }
 
-const CustomToolbar = ({ rowSelectionModel }: GridToolbarProps & { rowSelectionModel: GridRowSelectionModel }) => {
-  return (
-    <GridToolbarContainer>
-      {/* 
-      <GridToolbarDensitySelector /> */}
-      {/* <Box sx={{ flexGrow: 1 }} /> */}
-      {/* <GridToolbarExport /> */}
-    </GridToolbarContainer>
-  )
-}
+// const CustomToolbar = ({ rowSelectionModel }: GridToolbarProps & { rowSelectionModel: GridRowSelectionModel }) => {
+//   return (
+//     <GridToolbarContainer>
+//       {/*
+//       <GridToolbarDensitySelector /> */}
+//       {/* <Box sx={{ flexGrow: 1 }} /> */}
+//       {/* <GridToolbarExport /> */}
+//     </GridToolbarContainer>
+//   )
+// }
 
 ListPage.mapNavToOnRowClick = (nav: (id: number | string) => void) => (params: GridRowParams<any>) => nav(params.id)
 
