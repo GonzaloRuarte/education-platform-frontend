eecfbb7cdc29d50c395e3a895026cbc46b064df8
Nicolás Miñán <nm.imagenysonido@gmail.com>
2025-03-13 11:20:35 -0300

    updates for dockerization

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Meta Frontend
 
 Frontend layer for the Meta schools evaluation system
+
+#### Prestaging environment commands
+
+```bash
+docker compose -f 'docker-compose.pre_staging.yml' up -d --build
+```
--- mta_schools/components/CohortLevelChip.tsx
+++ mta_schools/components/CohortLevelChip.tsx
@@ -1,13 +1,11 @@
-import { cohortLevelCodeToLabel } from '@/mta_schools/constants'
-import { T_CohortLevel } from '@/mta_schools/types'
-import { Chip } from '@mui/material'
+// import { cohortLevelCodeToLabel } from '@/mta_schools/constants'
+// import { T_CohortLevel } from '@/mta_schools/types'
+// import { Chip } from '@mui/material'
 
+// interface I_Props {
+//   level: T_CohortLevel
+// }
 
-interface I_Props {
-  level: T_CohortLevel
-}
+// const CohortLevelChip = ({ level }: I_Props) => <Chip label={cohortLevelCodeToLabel(level)}></Chip>
 
-
-const CohortLevelChip = ({ level }: I_Props) => <Chip label={cohortLevelCodeToLabel(level)}></Chip>
-
-export default CohortLevelChip
+// export default CohortLevelChip
