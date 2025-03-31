5b82bf24cf607ff8e1c84b8a1abf2fcc26c815f7
Nicolás Miñán <nm.imagenysonido@gmail.com>
2025-03-31 10:57:33 -0300

    fix: remove debug log for API base path in config.ts

--- config.ts
+++ config.ts
@@ -1,7 +1,6 @@
 // Tables
 const PAGE_SIZE_OPTIONS = [10, 25, 100]
 const DEFAULT_PAGE_SIZE = 10
-console.log('process.env.NEXT_PUBLIC_API_BASE_PATH', process.env.NEXT_PUBLIC_API_BASE_PATH)
 /**
  * Returns full api urls
  * @param path REQUIRES leading slash
