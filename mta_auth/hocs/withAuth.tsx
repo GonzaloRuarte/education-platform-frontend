e5e8159084d87f6bedc3aad9f57427cdb1be30e3
Nicolás Miñán <nm.imagenysonido@gmail.com>
2025-04-14 17:56:48 -0300

    feat: enhance RequireAuth and withAuth HOCs to include logoutDestination; add navigation hooks for login redirection

--- mta_auth/hocs/withAuth.tsx
+++ mta_auth/hocs/withAuth.tsx
@@ -1,13 +1,19 @@
 import AvoidAuthorized from '@/mta_auth/hocs/AvoidAuthorized'
 import RequireAuth from '@/mta_auth/hocs/RequireAuth'
+import { T_LoginZone } from '../types'
 import { T_AllowedAccessGroups } from '@/mta_auth/types'
+import pages from '@/pages'
 import { ComponentProps, FC } from 'react'
 
-export const withAuth = (WrappedComponent: FC, allowedAccessGroups?: T_AllowedAccessGroups) => {
+export const withAuth = (
+  WrappedComponent: FC,
+  allowedAccessGroups?: T_AllowedAccessGroups,
+  logoutDestination: T_LoginZone = 'dashboard',
+) => {
   const WithAuthHOC = (props: ComponentProps<typeof WrappedComponent>) => {
     return (
       <>
-        <RequireAuth allowedAccessGroups={allowedAccessGroups} />
+        <RequireAuth allowedAccessGroups={allowedAccessGroups} logoutDestination={logoutDestination} />
         <WrappedComponent {...props} />
       </>
     )
