ce8069845db1195ac7fa019783c9ac593a728985
Nicolás Miñán <nm.imagenysonido@gmail.com>
2025-05-02 17:12:01 -0300

    feat: enhance ReportsListPage; simplify imports and improve URL rendering with a button

--- mta_reports/pages/ReportsListPage.tsx
+++ mta_reports/pages/ReportsListPage.tsx
@@ -3,21 +3,24 @@
 import { withAuth } from '@/mta_auth/hocs/withAuth'
 import { REPORT_NAME } from '@/mta_reports/constants'
 import { useReportList } from '@/mta_reports/hooks'
-import { SCHOOL_NAME } from '@/mta_schools/constants'
-import {
-  useNavigateToSchoolCreate,
-  useNavigateToSchoolDetail,
-  useSchoolBatchDelete,
-  useSchoolList,
-} from '@/mta_schools/hooks'
+import { useNavigateToSchoolCreate } from '@/mta_schools/hooks'
+import Button from '@/shared/components/Button'
 import ListPage from '@/shared/pages/ListPage'
-import { Chip } from '@mui/material'
 import { GridColDef } from '@mui/x-data-grid'
 
 const columns: Array<GridColDef> = [
   { field: 'id', headerName: '#' },
   { field: 'title', headerName: 'Título', flex: 1.5 },
-  { field: 'url', headerName: 'Link', flex: 1 },
+  {
+    field: 'url',
+    headerName: 'Link',
+    flex: 1,
+    renderCell: ({ row }) => (
+      <Button size="small" href={row.url}>
+        Ver reporte
+      </Button>
+    ),
+  },
 ]
 
 const ReportsListPage = () => {
