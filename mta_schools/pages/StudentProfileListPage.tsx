841b73deb7072e4ea6e6172851a00ee12878a399
Nicolás Miñán <nm.imagenysonido@gmail.com>
2025-05-02 14:25:50 -0300

    feat: enhance StudentProfileListPage; integrate school-specific profile listing and improve loading state handling

--- mta_schools/pages/StudentProfileListPage.tsx
+++ mta_schools/pages/StudentProfileListPage.tsx
@@ -1,14 +1,20 @@
 'use client'
 
 import { withAuth } from '@/mta_auth/hocs/withAuth'
+import { useUserProfilesResources } from '@/mta_auth/hooks'
 import { STUDENT_PROFILE_NAME } from '@/mta_schools/constants'
 import {
   useNavigateToStudentProfileBatchCreate,
   useNavigateToStudentProfileCreate,
   useNavigateToStudentProfileDetail,
   useStudentProfileList,
+  useStudentProfileListBySchool,
 } from '@/mta_schools/hooks'
+import { useSchoolOwnSchool } from '@/mta_schools/hooks/state'
+import { T_SchoolId } from '@/mta_schools/types'
 import Button from '@/shared/components/Button'
+import Spinner from '@/shared/components/Spinner'
+import { I_FetchOptions } from '@/shared/data/types'
 import ListPage from '@/shared/pages/ListPage'
 import { GridColDef } from '@mui/x-data-grid'
 
@@ -24,15 +30,25 @@ const columns: Array<GridColDef> = [
   { field: 'cohort', headerName: 'División', flex: 2 },
 ]
 
+const studentProfileListBySchool_CurrifiedHook = (schoolId: T_SchoolId) => (options?: I_FetchOptions) => {
+  return useStudentProfileListBySchool(options, { schoolId })
+}
+
 const StudentProfileListPage = () => {
   const navigateToDetail = useNavigateToStudentProfileDetail()
   const navigateToCreate = useNavigateToStudentProfileCreate()
   const navigateToBatchCreate = useNavigateToStudentProfileBatchCreate()
+  const { isAdmin } = useUserProfilesResources()
+  const ownSchool = useSchoolOwnSchool()
+
+  if (ownSchool === undefined) return <Spinner />
 
   return (
     <ListPage
       columns={columns}
-      useList={useStudentProfileList}
+      useList={
+        !isAdmin && ownSchool !== null ? studentProfileListBySchool_CurrifiedHook(ownSchool.id) : useStudentProfileList
+      }
       entityName={STUDENT_PROFILE_NAME}
       onCreate={navigateToCreate}
       onRowClick={ListPage.mapNavToOnRowClick(navigateToDetail)}
