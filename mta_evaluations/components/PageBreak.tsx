12345c431d53f3b6a87cba1d685b718215e93f65
Nicolás Miñán <nm.imagenysonido@gmail.com>
2025-05-07 19:45:45 -0300

    feat: add loading state management and error handling to PageBreak component

--- mta_evaluations/components/PageBreak.tsx
+++ mta_evaluations/components/PageBreak.tsx
@@ -4,14 +4,22 @@ import { DeleteButton } from '@/shared/components/buttons'
 import MagicGrid from '@/shared/components/MagicGrid'
 import Pastilla from '@/shared/components/Pastilla'
 import Spacer from '@/shared/components/Spacer'
+import { useInProgress } from '@/shared/hooks'
+import { handleServiceError } from '@/shared/service'
 import { T_VoidFn } from '@/shared/types'
 import { Grid2 as Grid } from '@mui/material'
 import { FC } from 'react'
 
 const PageBreak: FC<{ afterQuestionId: number; reload: T_VoidFn }> = ({ afterQuestionId, reload }) => {
   const remove = useRemovePageBreak()
+  const { setIsInProgress, setIsNotInProgress } = useInProgress()
+
   const handleDelete = () => {
-    remove({ after_question_id: afterQuestionId }).then(reload)
+    setIsInProgress()
+    remove({ after_question_id: afterQuestionId })
+      .then(reload)
+      .catch(handleServiceError)
+      .finally(() => setIsNotInProgress())
   }
   return (
     <>
