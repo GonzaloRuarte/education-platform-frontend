f65bdb69b1c9f6806104b3080359af5f80bc7b82
Nicolás Miñán <nm.imagenysonido@gmail.com>
2025-05-08 10:59:28 -0300

    feat: enhance DevDashboard with appointment management actions and UI components

--- mta_dev/hooks.ts
+++ mta_dev/hooks.ts
@@ -1,10 +1,13 @@
 import { useAuthResources } from '@/mta_auth/hooks'
+import { T_AppointmentId } from '@/mta_schedule/types'
 import { axiosPost } from '@/shared/data/axios'
 import { actionHookV3 } from '@/shared/hooks/dataServices/v3'
 import { T_EmptyPayload } from '@/shared/types'
 
-const DEV_PATH = '/development'
+// const DEV_PATH = '/development'
 const DEV_APPOINTMENT_FAKERIZE_PATH = '/development/appointment/fakerize/'
+const DEV_APPOINTMENT_MAKE_AVAILABLE_NOW_PATH = '/development/appointment/make-available-now/'
+const DEV_APPOINTMENT_SET_AS_FINISHED_PATH = '/development/appointment/set-as-finished/'
 
 const useDevAppointmentFakerize = actionHookV3<typeof DEV_APPOINTMENT_FAKERIZE_PATH, T_EmptyPayload, T_EmptyPayload>(
   DEV_APPOINTMENT_FAKERIZE_PATH,
@@ -12,4 +15,15 @@ const useDevAppointmentFakerize = actionHookV3<typeof DEV_APPOINTMENT_FAKERIZE_P
   useAuthResources,
 )
 
-export { useDevAppointmentFakerize }
+const useDevAppointmentMakeAvailableNow = actionHookV3<
+  typeof DEV_APPOINTMENT_MAKE_AVAILABLE_NOW_PATH,
+  { appointment_id: T_AppointmentId },
+  { message: string }
+>(DEV_APPOINTMENT_MAKE_AVAILABLE_NOW_PATH, axiosPost, useAuthResources)
+const useDevAppointmentSetAsFinished = actionHookV3<
+  typeof DEV_APPOINTMENT_SET_AS_FINISHED_PATH,
+  { appointment_id: T_AppointmentId },
+  { message: string }
+>(DEV_APPOINTMENT_SET_AS_FINISHED_PATH, axiosPost, useAuthResources)
+
+export { useDevAppointmentFakerize, useDevAppointmentMakeAvailableNow, useDevAppointmentSetAsFinished }
--- mta_dev/pages/DevDashboard.tsx
+++ mta_dev/pages/DevDashboard.tsx
@@ -1,16 +1,28 @@
 'use client'
 
 import { withAuth } from '@/mta_auth/hocs/withAuth'
-import { useDevAppointmentFakerize } from '@/mta_dev/hooks'
+import {
+  useDevAppointmentFakerize,
+  useDevAppointmentMakeAvailableNow,
+  useDevAppointmentSetAsFinished,
+} from '@/mta_dev/hooks'
+import { T_AppointmentId } from '@/mta_schedule/types'
 import DevButton from '@/shared/components/DevButton'
+import MagicGrid from '@/shared/components/MagicGrid'
 import Page from '@/shared/components/Page'
+import { H3, H4 } from '@/shared/components/Typography'
+import Input from '@/shared/forms/Input'
 import { useInProgress } from '@/shared/hooks'
 import { handleServiceError } from '@/shared/service'
-import { successToast } from '@/shared/toasts'
+import { errorToast, successToast } from '@/shared/toasts'
 import { Grid2 } from '@mui/material'
+import { useState } from 'react'
 
 const DevDashboard = () => {
   const { executeAction: appointmentFakerize } = useDevAppointmentFakerize()
+  const { executeAction: appointmentMakeAvailableNow } = useDevAppointmentMakeAvailableNow()
+  const { executeAction: appointmentSetAsFinished } = useDevAppointmentSetAsFinished()
+
   const actionHandler = (action: () => Promise<any>, message?: string) => {
     return () => {
       setIsInProgress()
@@ -23,38 +35,103 @@ const DevDashboard = () => {
     }
   }
   const { setIsInProgress, setIsNotInProgress } = useInProgress()
+  const [appointment_id, setAppointmentId] = useState<T_AppointmentId | ''>('')
+
   return (
-    <Page>
-      <Page.Title>Dashboard de Desarrollo</Page.Title>
-      <Page.Content>
-        <Grid2 container spacing={4}>
-          <Grid2 size={2}>
-            <DevButton size="large" title="python manage.py school_fakerize">
-              Crear escuela
-            </DevButton>
+    <>
+      <Page>
+        <Page.Title>Dashboard de Desarrollo</Page.Title>
+        <Page.Content>
+          <Grid2 container spacing={4}>
+            <Grid2 size={12}>
+              <H4>Escuelas</H4>
+            </Grid2>
+            <Grid2 size={3}>
+              <DevButton fullWidth size="large" title="python manage.py school_fakerize">
+                Crear escuela
+              </DevButton>
+            </Grid2>
+            <Grid2 size={3}>
+              <DevButton fullWidth size="large" title="python manage.py school_fakerize complete">
+                Crear escuela c/contenido
+              </DevButton>
+            </Grid2>
+
+            <Grid2 size={12}>
+              <H4>Evaluaciones</H4>
+            </Grid2>
+            <Grid2 size={3}>
+              <DevButton fullWidth size="large" title="python manage.py evaluations_fakerize complete">
+                Crear Evaluación
+              </DevButton>
+            </Grid2>
+            <Grid2 size={12}>
+              <H4>Turnos</H4>
+            </Grid2>
+            <Grid2 size={3}>
+              <DevButton
+                onClick={actionHandler(() => appointmentFakerize({}), 'Turno creado')}
+                fullWidth
+                size="large"
+                title="python manage.py appointment_fakerize"
+              >
+                Crear Turno
+              </DevButton>
+            </Grid2>
+            <Grid2 size={3}>
+              <MagicGrid>
+                <DevButton
+                  fullWidth
+                  size="large"
+                  title="python manage.py make_available_now <appointment_id>"
+                  onClick={actionHandler(() => {
+                    if (appointment_id === '') {
+                      errorToast('Por favor, proporcione un ID de turno válido.')
+                      return Promise.reject('ID de turno inválido')
+                    }
+                    return appointmentMakeAvailableNow({ appointment_id }).then((r) => successToast(r.message))
+                  })}
+                >
+                  Disponibilizar turno
+                </DevButton>
+                <Input
+                  size="small"
+                  type="number"
+                  value={appointment_id}
+                  onChange={(e) => setAppointmentId(Number(e.target.value))}
+                  label="Turno ID"
+                />
+              </MagicGrid>
+            </Grid2>
+            <Grid2 size={3}>
+              <MagicGrid>
+                <DevButton
+                  fullWidth
+                  size="large"
+                  title="python manage.py set_as_finished <appointment_id>"
+                  onClick={actionHandler(() => {
+                    if (appointment_id === '') {
+                      errorToast('Por favor, proporcione un ID de turno válido.')
+                      return Promise.reject('ID de turno inválido')
+                    }
+                    return appointmentSetAsFinished({ appointment_id }).then((r) => successToast(r.message))
+                  })}
+                >
+                  Finalizar turno
+                </DevButton>
+                <Input
+                  size="small"
+                  type="number"
+                  value={appointment_id}
+                  onChange={(e) => setAppointmentId(Number(e.target.value))}
+                  label="Turno ID"
+                />
+              </MagicGrid>
+            </Grid2>
           </Grid2>
-          <Grid2 size={2}>
-            <DevButton size="large" title="python manage.py school_fakerize complete">
-              Crear escuela (con contenido)
-            </DevButton>
-          </Grid2>
-          <Grid2 size={2}>
-            <DevButton size="large" title="python manage.py evaluations_fakerize complete">
-              Crear Evaluación
-            </DevButton>
-          </Grid2>
-          <Grid2 size={2}>
-            <DevButton
-              size="large"
-              title="python manage.py appointment_fakerize"
-              onClick={actionHandler(() => appointmentFakerize({}), 'Turno creado')}
-            >
-              Crear Turno
-            </DevButton>
-          </Grid2>
-        </Grid2>
-      </Page.Content>
-    </Page>
+        </Page.Content>
+      </Page>
+    </>
   )
 }
 
