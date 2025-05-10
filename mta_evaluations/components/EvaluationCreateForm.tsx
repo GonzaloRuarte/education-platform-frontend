d9352f03c2b4056d71f46f79265cdb2c26374ab7
Nicolás Miñán <nm.imagenysonido@gmail.com>
2025-05-10 14:39:43 -0300

    feat: enhance EvaluationCreateForm to include dynamic code generation and update input handling

--- mta_evaluations/components/EvaluationCreateForm.tsx
+++ mta_evaluations/components/EvaluationCreateForm.tsx
@@ -1,15 +1,16 @@
 'use client'
 
 import SubjectOptions from '@/mta_evaluations/components/SubjectOptions'
-import { useEvaluationCreate, useNavigateToEvaluationContentEdit } from '@/mta_evaluations/hooks'
+import { useEvaluationCreate, useEvaluationSubjects, useNavigateToEvaluationContentEdit } from '@/mta_evaluations/hooks'
 import { evaluationLabels } from '@/mta_evaluations/labels'
-import { EvaluationStatus, I_EvaluationCreateRequestData } from '@/mta_evaluations/types'
+import { EvaluationStatus, I_EvaluationCreateRequestData, I_EvaluationSubject } from '@/mta_evaluations/types'
 import { cleanPinnedText } from '@/mta_evaluations/utils'
 import { SchoolGradeSelectControlled } from '@/mta_schools/components/SchoolGradeSelect'
 import { SchoolGrade } from '@/mta_schools/constants'
 import MagicGrid from '@/shared/components/MagicGrid'
 import Spacer from '@/shared/components/Spacer'
 import Submit from '@/shared/components/Submit'
+import Input from '@/shared/forms/Input'
 import InputControlled from '@/shared/forms/InputControlled'
 import { rules } from '@/shared/forms/messages'
 import WysiwygEditorControlled from '@/shared/forms/WysiwygEditorControlled'
@@ -17,16 +18,18 @@ import { useInProgress } from '@/shared/hooks'
 import log from '@/shared/log'
 import { handleServiceError } from '@/shared/service'
 import { successToast } from '@/shared/toasts'
-import { SubmitHandler, useForm } from 'react-hook-form'
+import { randomInt } from '@/shared/utils'
+import { useMemo } from 'react'
+import { SubmitHandler, useForm, useWatch } from 'react-hook-form'
 
-interface I_FormFields extends Omit<I_EvaluationCreateRequestData, 'subject_id' | 'grade'> {
+interface I_FormFields extends Omit<I_EvaluationCreateRequestData, 'subject_id' | 'grade' | 'code'> {
   subject_id: I_EvaluationCreateRequestData['subject_id'] | null
   grade: I_EvaluationCreateRequestData['grade'] | null
 }
 
 const defaultValues: I_FormFields = {
   title: '',
-  code: '',
+  // code: '',
   header: '',
   pinned_text: null,
   status: EvaluationStatus.Draft,
@@ -35,6 +38,15 @@ const defaultValues: I_FormFields = {
 }
 const EvaluationCreateForm = () => {
   const { handleSubmit, control } = useForm<I_FormFields>({ defaultValues })
+  const subject_id = useWatch({ control, name: 'subject_id' })
+  const grade = useWatch({ control, name: 'grade' })
+  const subjects = useEvaluationSubjects()
+
+  const code = useMemo(() => {
+    if (subjects === undefined || subject_id === null || grade === null) return ''
+    const subject_prefix = (subjects.find((s) => s.id === subject_id) as I_EvaluationSubject).prefix
+    return `${subject_prefix}${grade}_${new Date().getFullYear()}_${randomInt(10000, 99999)}`
+  }, [subject_id, grade, subjects])
 
   const { setIsInProgress, setIsNotInProgress } = useInProgress()
   const navigateToEvaluationContentEdit = useNavigateToEvaluationContentEdit()
@@ -43,6 +55,7 @@ const EvaluationCreateForm = () => {
     setIsInProgress()
     evaluationCreate({
       ...data,
+      code,
       pinned_text: cleanPinnedText(pinned_text),
       subject_id: data.subject_id as string,
       grade: data.grade as SchoolGrade,
@@ -66,12 +79,7 @@ const EvaluationCreateForm = () => {
         />
         <SchoolGradeSelectControlled control={control} name="grade" rules={{ ...rules.required() }} />
         <SubjectOptions<I_FormFields> {...{ control }} name="subject_id" />
-        <InputControlled<I_FormFields>
-          {...{ control }}
-          name="code"
-          rules={{ ...rules.required() }}
-          label={evaluationLabels.code}
-        />
+        <Input name="code" label={evaluationLabels.code} disabled value={code} />
         <WysiwygEditorControlled<I_FormFields>
           {...{ control }}
           label={evaluationLabels.header}
