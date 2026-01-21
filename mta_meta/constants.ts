a068351aaca6c15202e4e8adfa39374c17564b23
GonzaloRuarte <gonzalorpg@gmail.com>
2026-01-21 14:00:27 -0300

    update types

--- mta_meta/constants.ts
+++ mta_meta/constants.ts
@@ -1,9 +1,10 @@
 import { EntityName } from '@/shared/utils'
 
-const META_REPORT_BUNDLE_NAME: EntityName = {
-  singular: 'Reporte META',
-  plural: 'Reportes META',
-  gs: ({ m, f }: { m: string; f: string }) => m, // default masculine for Spanish UI strings
-}
+const META_REPORT_BUNDLE_NAME = new EntityName({
+  singular: 'Reporte META+',
+  plural: 'Reportes META+',
+  gender: 'M',
+
+})
 
 export { META_REPORT_BUNDLE_NAME }
